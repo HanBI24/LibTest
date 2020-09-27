9091d6f2aaca9bdcbb7271262a322dc35a5eab8c
HanBI24 <alsltjdrhs@naver.com>
2020-09-27 21:54:01 +0900

    Lib test

---
+++ TestLib/src/main/java/com/example/testlib/ToastClass.java
@@ -0,0 +1,10 @@
+package com.example.testlib;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastClass {
+    public void showMyToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+}
